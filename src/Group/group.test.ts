0b14d96a787766c03c49e3a932cb84647ef0054e
Aleksandr Omelnitckii <omelniz@gmail.com>
2023-06-08 22:01:05 +0300

    test(group): Ceasar encrypt

--- src/Group/group.test.ts
+++ src/Group/group.test.ts
@@ -1,8 +1,15 @@
-import { it, expect } from "vitest";
-import { wallet } from "./index.js";
+import { it, expect, describe } from "vitest";
+import { wallet, encrypt, decrypt } from "./index.js";
 
 it("wallet group", () => {
 	expect(wallet.concat(wallet.empty, wallet.empty)).toBe(0);
 	expect(wallet.concat(1, wallet.inverse(1))).toBe(0);
 	expect(wallet.concat(100, wallet.concat(50, wallet.inverse(30)))).toBe(120);
+});
+
+describe("Ceasar encrypt", () => {
+	it("encrypt/decrypt", () => {
+		const text = encrypt("sdfsdfdsfdsf", 10);
+		expect(encrypt("sdfsdfdsfdsf", 10)).toEqual(decrypt(text, 10));
+	});
 });
--- src/Group/index.ts
+++ src/Group/index.ts
@@ -9,3 +9,31 @@ export const wallet: Group<number> = {
 	...addMonoid,
 	inverse: (a: number) => -a,
 };
+
+type Encrypt = (text: string, key: number) => string;
+type Decrypt = (text: string, key: number) => string;
+
+const abc = "abcdefghijklmnopqrstuvwxyz";
+
+const caesarGroup: Group<number> = {
+	empty: 0,
+	concat: (a, b) => (a + b) % abc.length,
+	inverse: (a) => (abc.length - a) % abc.length,
+};
+
+export const encrypt: Encrypt = (text, key) => {
+	return text
+		.toLowerCase()
+		.split("")
+		.map((char) => {
+			if (abc.indexOf(char) === -1) {
+				return char;
+			}
+
+			return caesarGroup.concat(abc.indexOf(char), key);
+		})
+		.join("");
+};
+
+export const decrypt: Decrypt = (text, key) =>
+	encrypt(text, caesarGroup.inverse(key));
